007544762f025527693c3bcbe122bc91670809f7
Osiel Sainz <osiel_oscar@hotmail.com>
2021-06-03 00:43:12 -0600

    Instalacion de Axios y bootstrap

--- proyecto-tickets/src/main.js
+++ proyecto-tickets/src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
